b91f1478117d4b1d8ad91a202c6b828107baf664
Valdecir Luciano <valdecirlucianoti@gmail.com>
2021-02-03 15:06:49 -0300

    cofigurando a integração de banco de dados POSTGRESQL

---
+++ src/main/java/com/eventosapp/model/Evento.java
@@ -0,0 +1,68 @@
+package com.eventosapp.model;
+
+import java.io.Serializable;
+
+public class Evento implements Serializable {
+	private static final long serialVersionUID = -3523013722928777791L;
+	private long codigo;
+	private String nome;
+	private String local;
+	private String data;
+	private String horario;
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLocal() {
+		return local;
+	}
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getHorario() {
+		return horario;
+	}
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (codigo ^ (codigo >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Evento other = (Evento) obj;
+		if (codigo != other.codigo)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Evento [codigo=" + codigo + ", nome=" + nome + ", local=" + local + ", data=" + data + ", horario="
+				+ horario + "]";
+	}
+	
+}
