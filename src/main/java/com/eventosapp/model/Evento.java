f5130ad031b8defab26e0dd80d3a24a47858226f
Valdecir Luciano <valdecirlucianoti@gmail.com>
2021-02-03 15:34:37 -0300

    implementando a classe Evento

--- src/main/java/com/eventosapp/model/Evento.java
+++ src/main/java/com/eventosapp/model/Evento.java
@@ -2,8 +2,17 @@ package com.eventosapp.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Evento implements Serializable {
 	private static final long serialVersionUID = -3523013722928777791L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long codigo;
 	private String nome;
 	private String local;
