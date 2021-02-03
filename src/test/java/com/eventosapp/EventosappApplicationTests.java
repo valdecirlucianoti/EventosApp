a6b44835afe119d0eb8cad48c64d64e25b6906b6
Valdecir Luciano <valdecirlucianoti@gmail.com>
2021-02-03 14:43:36 -0300

    Inicio do projeto de EventosApp

---
+++ src/test/java/com/eventosapp/EventosappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventosapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
