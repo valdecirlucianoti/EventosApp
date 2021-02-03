94cdf03905638f89b216e4d616325b5bc785f1a5
Valdecir Luciano <valdecirlucianoti@gmail.com>
2021-02-03 17:51:31 -0300

    implementado o model convidado e seu respectivo repository

---
+++ src/main/java/com/eventosapp/repository/ConvidadoRepository.java
@@ -0,0 +1,9 @@
+package com.eventosapp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.eventosapp.model.Convidado;
+
+public interface ConvidadoRepository extends CrudRepository<Convidado, String>{
+	
+}
