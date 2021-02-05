9ba1087de765a4f6c4cd4b374483e730de44cdb1
Valdecir Luciano <valdecirlucianoti@gmail.com>
2021-02-05 14:20:43 -0300

    implementando alterações BD heroku

--- src/main/java/com/eventosapp/configuration/DataConfigutarion_deploy_heroku_Postgres.java
+++ src/main/java/com/eventosapp/configuration/DataConfigutarion_deploy_heroku_Postgres.java
@@ -1,42 +1,30 @@
 package com.eventosapp.configuration;
 
 import java.net.URI;
-
-import javax.sql.DataSource;
-
+import java.net.URISyntaxException;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 @Profile("producao")
 public class DataConfigutarion_deploy_heroku_Postgres {
+	
 	@Bean
-	public BasicDataSource dataSource() {
+	public BasicDataSource dataSource() throws URISyntaxException {
 		URI dbUri = new URI(System.getenv("DATABASE_URL"));
-		String username = dbUri.getUserInfo();
-		
-		
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setUrl("jdbc:postgresql://localhost:5432/springbootbd");
-		dataSource.setUsername("postgres");
-		dataSource.setPassword("admin");
-		return dataSource;
-	}
 
-	@Bean
-	public JpaVendorAdapter jpaVendorAdapter() {
-		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-		adapter.setDatabase(Database.POSTGRESQL);
-		adapter.setShowSql(true);
-		adapter.setGenerateDdl(true);
-		adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
-		adapter.setPrepareConnection(true);
-		return adapter;
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath()
+				+ "?sslmode=require";
+
+		BasicDataSource basicDataSource = new BasicDataSource();
+		basicDataSource.setUrl(dbUrl);
+		basicDataSource.setUsername(username);
+		basicDataSource.setPassword(password);
+
+		return basicDataSource;
 	}
 }
